c59211edf82d2dccf5656679d888d9ce1eb78823
Royer G <royjuni3431@gmail.com>
2023-03-30 15:35:46 -0500

    feat: add basic styles in the Timer component

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -6,17 +6,17 @@ const Timer = () => {
       <div className="timer__content">
         <div className="timer__status">Work</div>
         <div className="timer__clock">
-          <div className="clock__hand--hour">
+          <div className="clock__hand clock__hand--hour">
             <span className="hand__caption">HH</span>
             <p className="hand__dial">00</p>
           </div>
           <span className="clock__splitter">:</span>
-          <div className="clock__hand--minute">
+          <div className="clock__hand clock__hand--minute">
             <span className="hand__caption">MM</span>
             <p className="hand__dial">00</p>
           </div>
           <span className="clock__splitter">:</span>
-          <div className="clock__hand--second">
+          <div className="clock__hand clock__hand--second">
             <span className="hand__caption">SS</span>
             <p className="hand__dial">00</p>
           </div>
@@ -24,8 +24,8 @@ const Timer = () => {
         <div className="timer__status">Rest</div>
       </div>
       <div className="timer-controls">
-        <button className="start">play</button>
-        <button className="stop">stop</button>
+        <button className="controls__button">play</button>
+        <button className="controls__button controls-button--deactivate">stop</button>
       </div>
     </div>
   )
